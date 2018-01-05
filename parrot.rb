5c67dea0378bc6e7503b75683bc65d030cfc2c44
Trac Nguyen <nguyendtrac@gmail.com>
2018-01-05 01:51:58 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(Squawk,phrase)
+def parrot("Squawk!",phrase)
   puts "#{phrase}"
-
+  return "Squawk!"
 end
